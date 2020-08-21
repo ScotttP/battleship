c205288dca958548a6ec78aebb46e9c1e00c9d9e
Scott Pribe <Scottpribe@gmail.com>
2020-08-20 23:18:33 -0400

    Updates

---
+++ precache-manifest.64c5b685f068f7152779c0eff463c1a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82764266ffbfad6296e42ec48503b217",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "eac2b01bd122e0132891",
+    "url": "/battleship/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a13106ef437d992c7f7a",
+    "url": "/battleship/static/js/2.8e3e921a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.8e3e921a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac2b01bd122e0132891",
+    "url": "/battleship/static/js/main.99383d47.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/battleship/static/media/logo.5d5d9eef.svg"
+  }
+]);
